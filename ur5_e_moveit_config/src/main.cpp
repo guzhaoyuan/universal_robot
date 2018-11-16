12ccdcacc20f2198b08ef07148b8108830d50b3e
guzhaoyuan <guzhaoyuan@126.com>
2018-11-16 01:43:21 -0500

    obstacle avoidance added

--- ur5_e_moveit_config/src/main.cpp
+++ ur5_e_moveit_config/src/main.cpp
@@ -1,3 +1,4 @@
+// moveit movegroup api add collision
 #include <moveit/move_group_interface/move_group_interface.h>
 #include <moveit/planning_scene_interface/planning_scene_interface.h>
 
--- ur5_e_moveit_config/src/planning_scene_ros.cpp
+++ ur5_e_moveit_config/src/planning_scene_ros.cpp
@@ -1,3 +1,5 @@
+// moveit ros api for adding collision
+
 #include <ros/ros.h>
 #include <geometry_msgs/Pose.h>
 
@@ -16,31 +18,18 @@
 
 int main(int argc, char** argv)
 {
-  ros::init(argc, argv, "planning_scene_ros_api_tutorial");
+  ros::init(argc, argv, "planning_scene_ros_api");
   ros::AsyncSpinner spinner(1);
   spinner.start();
 
   ros::NodeHandle node_handle;
-  // BEGIN_TUTORIAL
-  //
+
   // Visualization
-  // ^^^^^^^^^^^^^
-  // The package MoveItVisualTools provides many capabilties for visualizing objects, robots,
-  // and trajectories in RViz as well as debugging tools such as step-by-step introspection of a script
   moveit_visual_tools::MoveItVisualTools visual_tools("base_link");
   visual_tools.deleteAllMarkers();
 
-  // ROS API
-  // ^^^^^^^
-  // The ROS API to the planning scene publisher is through a topic interface
-  // using "diffs". A planning scene diff is the difference between the current
-  // planning scene (maintained by the move_group node) and the new planning
-  // scene desired by the user.
-  //
   // Advertise the required topic
-  // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-  // We create a publisher and wait for subscribers
-  // Note that this topic may need to be remapped in the launch file
+
   ros::Publisher planning_scene_diff_publisher = node_handle.advertise<moveit_msgs::PlanningScene>("planning_scene", 1);
   ros::WallDuration sleep_t(0.5);
   while (planning_scene_diff_publisher.getNumSubscribers() < 1)
@@ -49,11 +38,6 @@ int main(int argc, char** argv)
   }
   visual_tools.prompt("Press 'next' in the RvizVisualToolsGui window to start the demo");
 
-  // Define the attached object message
-  // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-  // We will use this message to add or
-  // subtract the object from the world
-  // and to attach the object to the robot
   moveit_msgs::AttachedCollisionObject attached_object;
   attached_object.link_name = "base_link";
   /* The header must contain a valid TF frame*/
@@ -76,26 +60,17 @@ int main(int argc, char** argv)
   primitive.dimensions[1] = 0.2;
   primitive.dimensions[2] = 0.3;
 
-
   attached_object.object.primitives.push_back(primitive);
   attached_object.object.primitive_poses.push_back(pose);
 
-  // Note that attaching an object to the robot requires
-  // the corresponding operation to be specified as an ADD operation
   attached_object.object.operation = attached_object.object.ADD;
 
-  // Add an object into the environment
-  // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-  // Add the object into the environment by adding it to
-  // the set of collision objects in the "world" part of the
-  // planning scene. Note that we are using only the "object"
-  // field of the attached_object message here.
   ROS_INFO("Adding the object into the world at the location of the hand.");
   moveit_msgs::PlanningScene planning_scene;
   planning_scene.world.collision_objects.push_back(attached_object.object);
   planning_scene.is_diff = true;
   planning_scene_diff_publisher.publish(planning_scene);
-  // visual_tools.prompt("Press 'next' in the RvizVisualToolsGui window to continue the demo");
+  visual_tools.prompt("Press 'next' in the RvizVisualTioolsGui window to continue the demo");
 
   ros::shutdown();
   return 0;
